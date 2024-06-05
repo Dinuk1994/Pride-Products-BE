e1e2d3d5a110987a5790ce982598ea79f5ccd598
Dinuk1994 <ddt94119@gmail.com>
2024-06-06 01:02:22 +0530

    connected to the mongoDB

---
+++ server.jsx
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(fileUpload({
+    useTempFiles: true
+}));
+
+// Connect to MongoDB
+const connectDB = async () => {
+    try {
+        const URI = process.env.MONGODB_URL;
+        await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to DB");
+    } catch (err) {
+        console.error(err);
+        process.exit(1); 
+    }
+};
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.json({ msg: "Welcome to Pride Products" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Server is up and running', PORT);
+});

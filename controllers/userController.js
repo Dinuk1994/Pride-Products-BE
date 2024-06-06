0d80b8bfa4bdc74909e1c7a06560efa5416820ef
Dinuk1994 <ddt94119@gmail.com>
2024-06-07 01:46:16 +0530

    Register a user complete BE

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,27 @@
 const users = require('../models/userModel')
+const bcrypt = require('bcrypt')
 
 const userController ={
-    register :(req, res)=>{
-        res.json({msg : "test controller"})
+    register :async (req, res)=>{
+       try {
+        const{name,email,password} = req.body
+        const user = await users.findOne({email})
+        if(user) return res.status(400).json({msg :"this email already exists" })
+
+        if(password.length < 6)
+        return res.status(400).json({msg :"password must be at least 6 characters" }) 
+
+        const passwordHash = await bcrypt.hash(password,10)
+
+        const newUser = new users({
+            name,email,password:passwordHash
+        })  
+        await newUser.save();
+        return res.json({msg : "Register Successfull"})
+
+       } catch (error) {
+        return res.status(500).json({msg : error.message}) 
+       }    
     }
 }
 
--- models/userModel.js
+++ models/userModel.js
@@ -9,7 +9,8 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         require: true,
-        trim: true
+        trim: true,
+        unique : true
     },
     password: {
         type: String,

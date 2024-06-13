aa6f6a3c036390a4d37abe17eeb137465f46a53e
Dinuk1994 <ddt94119@gmail.com>
2024-06-14 01:57:34 +0530

    Temp file removed applied

--- routers/upload.js
+++ routers/upload.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const cloudinary = require('cloudinary')
 const auth = require('../middlewares/auth')
 const authAdmin = require('../middlewares/authAdmin')
+const fs = require('fs')
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -19,15 +20,18 @@ router.post('/upload', (req, res) => {
         const file = req.files.file ? req.files.file : req.files.undefined;
 
         if (file.size >10* 1024 * 1024) {
+            removeTemp(file.tempFilePath)
             return res.status(400).json({ msg: "File is too large" });
         }
 
         if(file.mimetype !=='image/jpeg' && file.mimetype !== 'image/png') {
+            removeTemp(file.tempFilePath)
             return res.status(400).json({ msg: "File format is incorrect" });
         }
 
         cloudinary.v2.uploader.upload(file.tempFilePath, {folder:'test'},async(err,result)=>{
             if(err) throw err;
+            removeTemp(file.tempFilePath)
             return res.json({public_id:result.public_id, url:result.secure_url});
         })
 
@@ -35,5 +39,11 @@ router.post('/upload', (req, res) => {
         res.status(500).json({ msg: err.message });
     }
 })
+
+const removeTemp=(path)=>{
+    fs.unlink(path, err=>{
+        if(err) throw err
+    })
+}
 
 module.exports = router;

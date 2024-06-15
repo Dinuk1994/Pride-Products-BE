76f1432aa17a62635eb21c705a7c26307396d362
Dinuk1994 <ddt94119@gmail.com>
2024-06-15 18:40:32 +0530

    Completed the CRUD of products

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,24 +4,25 @@ const productController = {
 
     getProducts: async (req, res) => {
         try {
-           
+            const product = await products.find()
+            return res.json(product )
 
-        } catch (error) {
+        } catch (err) {
             return res.status(500).json({ msg: err.message })
         }
     },
     createProducts: async (req, res) => {
         try {
-            const { product_id,productName, price, description, images, category,checked } = req.body;
+            const { product_id, productName, price, description, images, category, checked } = req.body;
 
             if (!images) return res.status(400).json({ msg: "No image uploaded" })
 
             const product = await products.findOne({ product_id })
             if (product) return res.status(400).json({ msg: "This product already exists" })
 
-            const newProduct = new products({ product_id, productName, images, category, description, price ,checked })
+            const newProduct = new products({ product_id, productName, images, category, description, price, checked })
             newProduct.save();
-            return res.json({ msg : "Product saved successsfully" })
+            return res.json({ newProduct })
 
         } catch (err) {
             return res.status(500).json({ msg: err.message })
@@ -30,7 +31,10 @@ const productController = {
     },
     updateProducts: async (req, res) => {
         try {
-            
+            const { productName, images, category, description, price, checked } = req.body
+            if (!images) return res.status(400).json({ msg: "No image uploaded" })
+            await products.findByIdAndUpdate({ _id: req.params.id }, { productName, images, category, description, price, checked })
+            return res.json({ msg: "Product update success" })
 
         } catch (err) {
             return res.status(500).json({ msg: err.message })
@@ -39,8 +43,8 @@ const productController = {
     },
     deleteProducts: async (req, res) => {
         try {
-            const product = await products.findByIdAndDelete(req.params.id)
-            res.json({msg : "Product Deleted"})
+            await products.findByIdAndDelete(req.params.id)
+            res.json({ msg: "Product Deleted" })
         } catch (err) {
             return res.status(500).json({ msg: err.message })
 
